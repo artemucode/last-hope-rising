9c478041e97666db370ba89b4bad1c243bf725f0
Artem <usoaniartem35@gmail.com>
2026-07-28 08:49:14 +0300

    Добавил анимации для игрока

--- main_menu.gd
+++ main_menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://main.tscn")
+	get_tree().change_scene_to_file("res://level1.tscn")
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
--- player.gd
+++ player.gd
@@ -1,33 +1,60 @@
 extends CharacterBody2D
 
+@onready var anim = $AnimatedSprite2D
+
 var speed = 200
-var jumpForce = 300  # Исправлено опечаточное название (было jumpForse)
+var jump_force = 300
 var gravity = 900
 
-# vel не нужна, используем встроенную velocity
 
-@onready var imagePlayer = get_node("player")
+func _ready():
+	anim.play("idle")
+
 
 func _physics_process(delta):
-    # 1. Применяем гравитацию (если не на полу)
-    if not is_on_floor():
-        velocity.y += gravity * delta
 
-    # 2. Горизонтальное движение (сброс перед вводом)
-    velocity.x = 0
-    if Input.is_action_pressed("player_left"):
-        velocity.x -= speed
-    elif Input.is_action_pressed("player_right"):
-        velocity.x += speed
+	# Гравитация
+	if not is_on_floor():
+		velocity.y += gravity * delta
 
-    # 3. Прыжок
-    if Input.is_action_just_pressed("player_jump") and is_on_floor():
-        velocity.y = -jumpForce
 
-    # 4. Движение
-    move_and_slide()
+	# Движение
+	velocity.x = 0
 
-    if velocity.x < 0:
-        imagePlayer.flip_h = true
-    elif velocity.x > 0:
-        imagePlayer.flip_h = false
+	if Input.is_action_pressed("player_left"):
+		velocity.x = -speed
+
+	elif Input.is_action_pressed("player_right"):
+		velocity.x = speed
+
+
+	# Прыжок
+	if Input.is_action_just_pressed("player_jump") and is_on_floor():
+		velocity.y = -jump_force
+
+
+	# Анимации
+
+	# Если персонаж в воздухе
+	if not is_on_floor():
+		anim.play("jump")
+
+
+	# Если идёт
+	elif velocity.x != 0:
+		anim.play("walk")
+
+		# Поворот спрайта
+		if velocity.x < 0:
+			anim.flip_h = true
+		else:
+			anim.flip_h = false
+
+
+	# Если стоит
+	else:
+		anim.play("idle")
+
+
+	# Движение
+	move_and_slide()
